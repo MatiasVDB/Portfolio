9204c07da4b8e276ddab1718d58da0e25f3b0850
MatiasVDB <matiasvdb@hotmail.com>
2022-10-05 16:20:35 -0300

    Fixed bugs, refactorized code.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,10 @@
-var savedPositionTop = 0;
-var savedPositionLeft = 0;
-$('#windows-start-button').click(function () {
-    
-    $('.start-bar').show();
-
-});
+const hashtag = '#';
+var savedPositionTop = '';
+var savedPositionLeft = '';
+var idBtnRestore = '';
 
 function openWindow(idWindow, idButtonFromTaskbarToShow) {
-  
+
   $(idWindow).show();
   $(idButtonFromTaskbarToShow).show();
   $(idWindow).css("z-index", "10000");
@@ -15,94 +12,94 @@ function openWindow(idWindow, idButtonFromTaskbarToShow) {
 }
 
 function minimizeWindow(idWindowToMinimize) {
-  
+
   $(idWindowToMinimize).hide();
+
+  $(idWindowToMinimize).removeClass("showed").addClass("hidded");
 
 }
 
-function maximizeWindow(idWindow, idBtnMaximize, idBtnRestore) {
+function selectorSizeWindow(window, e) {
 
-  if ($('#' + idBtnMaximize).length) {
+  if ($(e).attr('aria-label') === 'Maximize') {
 
-    savedPositionTop = $('#' + idWindow).css("top");
-    savedPositionLeft = $('#' + idWindow).css("left");
+    maximizeWindow(window, e);
 
-    $('#'+idBtnMaximize).attr('aria-label', 'Restore');
-
-    $('#' + idBtnMaximize).attr('id', idBtnRestore);
-
-    $('#' + idWindow).css("width", "100%");
-
-    $('#' + idWindow).css("height", "96%");
-    
-    $('#' + idWindow).css("position", "fixed");
-
-    $('#' + idWindow).css("top", "0px");
-
-    $('#' + idWindow).css("left", "0px");
-
-    $('#' + idWindow).css("transform", "none");
-
-    if (idBtnMaximize === "maximize") {
-
-      $("#photo").css("width", "60em");
-      $("#photo").css("height", "77em");
-    }
-
-    if (idBtnMaximize === "maximize-about-me") {
-      
-      $("textarea").css("height", "78em");
-    }
-
-    if (idBtnMaximize === "maximize-internet-explorer") {
-      
-      $("iframe").css("height", "1000px");
-    }
   }
 
   else {
 
-    $('#' + idWindow).css("width", "40%");
-    
-    $('#'+idWindow).css("height", "auto");
+    restoreWindow(window, e);
+  }
+}
 
-    $('#'+idBtnRestore).attr('aria-label', 'Maximize');
+function maximizeWindow(window, obj) {
 
-    $('#' + idBtnRestore).attr('id', idBtnMaximize);
+  idBtnRestore = obj;
+  savedPositionTop = $(hashtag + window).css("top");
+  savedPositionLeft = $(hashtag + window).css("left");
 
-    $('#' + idWindow).css("top", savedPositionTop);
+  $(obj).attr('aria-label', "Restore");
 
-    $('#' + idWindow).css("left", savedPositionLeft);
-    
-    $('#' + idWindow).css("transform", "translate(-50%, -50%)");
+  $(hashtag + window).css("width", "100%");
 
-    if (idBtnMaximize === "maximize") {
+  $(hashtag + window).css("position", "fixed");
 
+  $(hashtag + window).css("top", "0px");
+
+  $(hashtag + window).css("left", "0px");
+
+  $(hashtag + window).css("transform", "none");
+
+  switch (window) {
+    case 'window':
+      $("#photo").css("width", "60em");
+      $("#photo").css("height", "77em");
+      break;
+    case 'window-about-me':
+      $("textarea").css("height", "78em");
+      break;
+    case 'window-internet-explorer':
+      $("iframe").css("height", "1000px");
+      break;
+    default:
+      $(hashtag + window).css("height", "96%");
+  }
+}
+
+function restoreWindow(window, obj) {
+
+  $(obj).attr('aria-label', 'Maximize');
+
+  $(hashtag + window).css("width", "40%");
+
+  $(hashtag + window).css("top", savedPositionTop);
+
+  $(hashtag + window).css("left", savedPositionLeft);
+
+  $(hashtag + window).css("transform", "translate(-50%, -50%)");
+
+  switch (window) {
+    case 'window':
       $("#photo").css("width", "400px");
       $("#photo").css("height", "auto");
-      $('#' + idWindow).css("transform", "none");
-    }
-
-    if (idBtnMaximize === "maximize-about-me") {
-      
+      $(hashtag + window).css("transform", "none");
+      break;
+    case 'window-about-me':
       $("textarea").css("height", "auto");
-      $('#' + idWindow).css("transform", "none");
-    }
-
-    if (idBtnMaximize === "maximize-internet-explorer") {
-      
+      $(hashtag + window).css("transform", "none");
+      break;
+    case 'window-internet-explorer':
       $("iframe").css("height", "500px");
-      
-    }
-    
-
+      break;
+    default:
+      $(hashtag + window).css("height", "auto");
   }
-
 }
 
 
 function closeWindow(idWindowToClose, idButtonFromTaskbarToClose) {
-  
+
   $(idWindowToClose).hide();
 
   $(idButtonFromTaskbarToClose).hide();
@@ -111,99 +108,78 @@ function closeWindow(idWindowToClose, idButtonFromTaskbarToClose) {
 function btnShowOrHideWindow(idWindowToShowOrHide) {
 
   if ($(idWindowToShowOrHide).hasClass("showed")) {
-    
-    $(idWindowToShowOrHide).toggleClass("showed").addClass("hidded");
 
     $(idWindowToShowOrHide).hide();
+    $(idWindowToShowOrHide).removeClass("showed").addClass("hidded");
 
-    }
+  }
 
-    else{
+  else {
 
-      $(idWindowToShowOrHide).show();
-      $(idWindowToShowOrHide).addClass("showed");
+    $(idWindowToShowOrHide).show();
+    $(idWindowToShowOrHide).removeClass("hidded").addClass("showed");
+    $(idWindowToShowOrHide).css("z-index", "10000");
 
-    }
+  }
 
 }
 
-function moveElementWithMouse(id, idBtnMaximize, idBtnRestore) {
+function moveElementWithMouse(id) {
 
   var mousePosition;
-  var offset = [0,0];
+  var offset = [0, 0];
   var w;
   var isDown = false;
   var windows;
-
   var counter = 1;
-      
+
   w = document.getElementById(id);
-  
+
   windows = document.getElementsByClassName('window');
 
   w.addEventListener('mousedown', function (e) {
 
-    for(var i =0, il = windows.length;i<il;i++){
+    for (var i = 0, il = windows.length; i < il; i++) {
       windows[i].style.zIndex = 0;
     }
 
     w.style.zIndex = ++counter;
     isDown = true;
-      offset = [
-          w.offsetLeft - e.clientX,
-          w.offsetTop - e.clientY
+    offset = [
+      w.offsetLeft - e.clientX,
+      w.offsetTop - e.clientY
     ];
-   
-    
+
+
   }, true);
-  
+
   document.addEventListener('mouseup', function () {
     isDown = false;
   }, true);
-  
-  document.addEventListener('mousemove', function(event) {
+
+  document.addEventListener('mousemove', function (event) {
     event.preventDefault();
-    
+
     if (isDown) {
 
-      if ($('#' + idBtnRestore).length) {
-      
-        $('#' + id).css("width", "40%");
-    
-        $('#' + id).css("height", "auto");
+      if ($(idBtnRestore).attr('aria-label') === 'Restore') {
 
-        $('#'+idBtnRestore).attr('aria-label', 'Maximize');
+        if (id !== 'window-technologie-info') {
 
-        $('#' + idBtnRestore).attr('id', idBtnMaximize);
-        
-        if (idBtnMaximize === "maximize") {
+          restoreWindow(id, idBtnRestore);
 
-          $("#photo").css("width", "400px");
-          $("#photo").css("height", "auto");
         }
-
-        if (idBtnMaximize === "maximize-about-me") {
-          
-          $("textarea").css("height", "auto");
-        }
-
-        if (idBtnMaximize === "maximize-internet-explorer") {
-          
-          $("iframe").css("height", "500px");
-        }
-
-      }
-
+      }   
       mousePosition = {
-  
-          x : event.clientX,
-          y : event.clientY
-  
-    };
 
-    w.style.left = (mousePosition.x + offset[0]) + 'px';
-    w.style.top = (mousePosition.y + offset[1]) + 'px';
-          
+        x: event.clientX,
+        y: event.clientY
+
+      };
+
+      w.style.left = (mousePosition.x + offset[0]) + 'px';
+      w.style.top = (mousePosition.y + offset[1]) + 'px';
+
     }
 
   }, true);
@@ -215,23 +191,23 @@ function startTime() {
   var time = new Date();
 
   $(".time").css("margin", "0px");
-  
+
   $(".time").html(time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
-  
+
   setTimeout(startTime, 1000);
-  
+
 }
 
 $('#window-technologies li').click(function () {
 
-  var hasNumber = /\d/;   
- 
+  var hasNumber = /\d/;
+
   openWindow("#window-technologie-info", null);
 
   $(".web-lenguage").attr("href", $(this).children().attr("data-url"));
 
   $(".icon-lenguage").attr("src", $(this).children().attr("src"));
-  
+
   if ($(this).children().attr("data-year").includes("1")) {
 
     $(".time-experiencie").html($(this).children().attr("data-year") + "" + " year");
@@ -241,23 +217,23 @@ $('#window-technologies li').click(function () {
   else {
 
     if (!hasNumber.test($(this).children().attr("data-year"))) {
-      
+
       $(".time-experiencie").html($(this).children().attr("data-year"));
     }
 
     else {
-      
+
       $(".time-experiencie").html($(this).children().attr("data-year") + "" + " years");
 
     }
-    
+
   }
 
 });
-  
 
 
 
 
 
-  
+
+
